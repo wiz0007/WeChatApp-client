1829962cf8a2f092c04f839cb33a36a76990109c
Ayushmaan Mishra <ayush8171wiz@gmail.com>
2026-05-03 23:54:39 +0530

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL =
-  "http://localhost:5000";
+  "https://wechat-server-w3bp.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
